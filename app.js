2f9a2d2b981d3bde2afd7de61d48b541099c1379
juliangiansanti <juliangiansanti@gmail.com>
2025-07-31 01:14:53 -0300

    Entrega numero 2 de JS

---
+++ app.js
@@ -0,0 +1,41 @@
+let turnos = [];
+
+let ultimoId = 1;
+
+function nuevoTurno (){
+    const nombre = document.getElementById('nombre').value;
+    const fecha = document.getElementById('fecha').value;
+    const hora = document.getElementById('hora').value;
+
+    const turno = {
+        id: ultimoId++,
+        nombre: nombre,
+        fecha: fecha,
+        hora: hora,
+    }
+
+    turnos.push(turno);
+    alert('El turno ha sido asignado con exito!');
+}
+
+document.getElementById('turnosForm').addEventListener('submit', function(event){
+    event.preventDefault();
+    nuevoTurno();
+    mostrarTurnos();
+})
+
+function mostrarTurnos(){
+    turnosLista.innerHTML = "";
+
+    turnos.forEach((turno) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <p><strong>ID:</strong> ${turno.id}</p>
+        <p><strong>Nombre:</strong> ${turno.nombre}</p>
+        <p><strong>Fecha:</strong> ${turno.fecha}</p>
+        <p><strong>Hora:</strong> ${turno.hora}</p>
+        `;
+
+        turnosLista.appendChild(div);
+    })
+}
